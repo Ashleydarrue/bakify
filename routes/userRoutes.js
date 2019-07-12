b20af4fc06acb4dedd9059ade788fcb463d63057
Ashley Arrue <ashleydarrue@gmail.com>
2019-07-12 18:01:30 -0400

    sign up button is functional and creates user to the database

---
+++ routes/userRoutes.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const router  = express.Router();
+const User    = require('../models/User');
+const bcrypt  = require('bcryptjs');
+const passport = require('passport');
+const ensureLogin = require("connect-ensure-login");
+
+
+
+router.post('/signup', (req, res, next)=>{
+  const thePassword = req.body.password;
+  const theUsername = req.body.username;
+  const theDate = req.body.eventDate;
+  const salt = bcrypt.genSaltSync(12);
+  const hashedPassWord =  bcrypt.hashSync(thePassword, salt);
+  
+      User.create({
+          username: theUsername,
+          password: hashedPassWord,
+          eventDate: theDate,
+      })
+      .then(()=>{
+          console.log('yay');
+          res.redirect('/')
+      })
+      .catch((err)=>{
+          next(err);
+      })
+})
+
+router.get("/profile", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("userViews/profile", { user: req.user });
+});
+
+module.exports = router;
